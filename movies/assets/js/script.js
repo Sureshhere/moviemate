6548017c2e53b8b4e936eb44a1e01689521701ec
Sureshhere <97615073+Sureshhere@users.noreply.github.com>
2022-06-09 21:05:12 +0530

    Update script.js

--- movies/assets/js/script.js
+++ movies/assets/js/script.js
@@ -114,3 +114,22 @@ $(document).keydown(function(e) {
         return false;
     }
 });
+
+// *********ad block*******************
+
+
+
+window.onload = function() {
+    var button = document.getElementsByName("sandbox")[0]
+    var iframe = document.getElementsByName("framez")[0]
+    button.addEventListener('click', sndbx, false);
+
+    function sndbx() {
+        var nibba = document.getElementById("framez").src;
+        if (iframe.sandbox == 'allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation') {
+            document.getElementById("framez").removeAttribute("sandbox");
+        }
+        frames['framez'].location.href = nibba;
+        iframe.sandbox = 'allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation';
+    }
+}
